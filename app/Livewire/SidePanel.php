b7ff8a3732cdd83d5b8d5c82a7252345f2e59551
vit-posmourny <dr1v3r@email.cz>
2024-12-13 08:39:52 +0100

    mam pridanej jako full page komponentu

--- app/Livewire/SidePanel.php
+++ app/Livewire/SidePanel.php
@@ -1,25 +1,27 @@
 <?php
-
 namespace App\Livewire;
 
 use App\Models\Place;
-use Illuminate\Http\Request;
 use Livewire\Component;
 
 class SidePanel extends Component
 {
+    
+    public $latitude = null;
+    public $longitude = null;
+    public $elevation = null;
 
-    public $count = 0;
-
-    public function increment()
-    {
-        $this->count++;
-    }
 
     public function store()
     {
-       
+        Place::create([
+
+            'latitude' => $this->latitude,
+            'longitude' => $this->longitude,
+            'elevation' => $this->elevation,
+        ]);
     }
+
 
     public function render()
     {
--- resources/views/livewire/side-panel.blade.php
+++ resources/views/livewire/side-panel.blade.php
@@ -2,18 +2,18 @@
     // jsem livewire side panel
     <div class="w-1/4 shadow-2xl p-8 text-xl font-medium overflow-hidden min-w-96">
 
-    <!-- <form action="/elevation/store" method="POST" class="flex flex-col gap-8"> -->
-        @csrf
-        <div class="grid grid-cols-8 items-baseline align-between gap-4 ooverflow-hidden">
-            <span wire:model ="SidePanel">{{$count}}</span>
-            <span class="select-none">Lat:</span><x-text-input id="i-latitude" type="text" name="latitude" class="col-span-7"/>
-            <span class="select-none">Lon:</span><x-text-input id="i-longitude" type="text" name="longitude" class="col-span-7"/> 
-            <span class="select-none col-span-3">Elevation:</span><x-text-input id="i-elevation" type="text" name="elevation" class="col-span-4 text-right"/><span class="select-none col-span-1 text-right">m</span>
-        </div>
+        <form wire:submit.prevent="store" class="flex flex-col gap-8">
+            <!-- @csrf -->
+            <div class="grid grid-cols-8 items-baseline align-between gap-4 ooverflow-hidden">
+                <span class="select-none">Lat:</span><x-text-input id="latitude" type="text" wire:model="latitude" name="latitude" class="col-span-7"/>
+                <span class="select-none">Lon:</span><x-text-input id="longitude" type="text" wire:model="longitude" name="longitude" class="col-span-7"/> 
+                <span class="select-none col-span-3">Elevation:</span><x-text-input id="elevation" type="text" wire:model="elevation" name="elevation" class="col-span-4 text-right"/><span class="select-none col-span-1 text-right">m</span>
+            </div>
 
-            <button wire:click = "increment">Store to database</button>
+                <x-primary-button type="submit">Store to database</x-primary-button>
 
-    <!-- </form> -->
+        </form>
 
-</div>
+    </div>
+    
 </div>
