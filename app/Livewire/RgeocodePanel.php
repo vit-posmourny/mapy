52511e2ba1fc179c9e1ac605dd8838868e2bba60
vit-posmourny <dr1v3r@email.cz>
2025-01-05 00:50:14 +0100

    uklada do databaze :-)

--- app/Livewire/RgeocodePanel.php
+++ app/Livewire/RgeocodePanel.php
@@ -7,30 +7,68 @@ use Livewire\Component;
 
 class RgeocodePanel extends Component
 {
-    public $label = null;
-    public $location = null;
-    public $name = null;
-    public $name = null;
-    public $name = null;
-    public $name = null;
-    public $name = null;
-    public $zip = null;
+    public $label = '';
+    public $location = '';
+    public $name = '';
+    public $latitude = null;
+    public $longitude = null;
+    public $regional_address = '';
+    public $regional_street = '';
+    public $regional_municipality_part_1 = '';
+    public $regional_municipality_part_2 = '';
+    public $regional_municipality = '';
+    public $regional_region_1 = '';
+    public $regional_region_2 = '';
+    public $regional_country = '';
+    public $isoCode = '';
+    public $zip = '';
+
+
+    protected $listeners = [
+        'values-updated' => 'handleUserUpdate',
+    ];
+
+
+    public function handleUserUpdate($label, $location, $name, $lat, $lon, $regional_address, 
+        $regional_street, $regional_municipality_part_1, $regional_municipality_part_2, 
+        $regional_municipality, $regional_region_1, $regional_region_2, $regional_country, $isoCode, $zip)
+    {
+        $this->latitude = $lat;
+        $this->longitude = $lon;
+        $this->regional_address = $regional_address;
+        $this->regional_street = $regional_street;
+        $this->regional_municipality_part_1 = $regional_municipality_part_1;
+        $this->regional_municipality_part_2 = $regional_municipality_part_2;
+        $this->regional_municipality = $regional_municipality;
+        $this->regional_region_1 = $regional_region_1;
+        $this->regional_region_2 = $regional_region_2;
+        $this->regional_country = $regional_country;
+        $this->isoCode = $isoCode;
+        $this->label = $label;
+        $this->location = $location;
+        $this->name = $name;
+        $this->zip = $zip;
+    }   
 
     public function store()
     {
         $validated = $this->validate([ 
             
-            'latitude' => 'required|numeric|max:360',
-            'longitude' => 'required|numeric|max:360',
-            'elevation' => 'required|numeric|max:9999',
-        ],[ // pomocí příkazu php artisan vendor:publish --tag=lang
-            // mohu vytáhnout texty s eng a lokalizovat si je do cs
-            'latitude.numeric' => "Hodnota v poli musí být číselná.",
-            'longitude.numeric' => "Hodnota v poli musí být číselná.",
-            'elevation.numeric' => "Hodnota v poli musí být číselná.",
-            'latitude.required' => "Pole nesmí zůstat prázdně.",
-            'longitude.required' => "Pole nesmí zůstat prázdně.",
-            'elevation.required' => "Pole nesmí zůstat prázdně.",
+            'label' => 'string|max:255',
+            'location' => 'string|max:255',
+            'name' => 'string|max:255',
+            'latitude' => 'numeric|max:360',
+            'longitude' => 'numeric|max:360',
+            'regional_address' => 'string|max:255',
+            'regional_street' => 'string|max:255',
+            'regional_municipality_part_1' => 'string|max:255',
+            'regional_municipality_part_2' => 'string|max:255',
+            'regional_municipality' => 'string|max:255',
+            'regional_region_1' => 'string|max:255',
+            'regional_region_2' => 'string|max:255',
+            'regional_country' => 'string|max:255',
+            'isoCode' => 'string|max:255',
+            'zip' => 'string|max:6',
         ]);
 
         //znovu nastaví hodnoty všech props.komponenty do init.stavu tj.null
--- public/js/rgeocode.js
+++ public/js/rgeocode.js
@@ -52,10 +52,26 @@ map.on('click', async function mapClick(e) {
     let lat = e.latlng.lat;
     let lon = e.latlng.lng;
 
-	let lat_elem = document.getElementById('i-label');
+	let lab_elem = document.getElementById('i-label');
 	let loc_elem = document.getElementById('i-location');
 	let name_elem = document.getElementById('i-name');
 	let zip_elem = document.getElementById('i-zip');
+
+	lab_elem.value = '';
+	loc_elem.value = '';
+	name_elem.value = '';
+	zip_elem.value = '';
+	let lat_elem = null;
+	let lon_elem = null;
+	let regional_address = '';
+    let regional_street = ''; 
+    let regional_municipality_part_1 = '';
+    let regional_municipality_part_2 = '';
+    let regional_municipality = '';
+    let regional_region_1 = '';
+    let regional_region_2 = '';
+    let regional_country = '';
+    let isoCode = '';
 
 	try {
 		const response = await fetch(`https://api.mapy.cz/v1/rgeocode/?lon=${lon}&lat=${lat}&apikey=${API_KEY}`, {
@@ -64,34 +80,76 @@ map.on('click', async function mapClick(e) {
 		const json = await response.json();
 
 		if (json?.items?.length > 0) {
-			html = '<p>Response details are available in the console.</p><ul>';
-			json.items.forEach(item => {
-				html += `<li>${item.label}</li>`;
-				html += `<li>${item.location}</li>`;
-				html += `<li>${item.name}</li>`;
-				html += `<li>${item.position.lat}</li>`;
-				html += `<li>${item.position.lon}</li>`;
-				item.regionalStructure.forEach(index => {
-					html += `<li>${index.name}</li>`;
-					html += `<li>${index.type}</li>`;
-					if (index.type == 'regional.country'){
-						html += `<li>${index.isoCode}</li>`;
-					}
-				})
-			});
-			html += '</ul>';
 
+			json.items.forEach(item => {
+				// položky v rgeocode-panelu
+				lab_elem.value = item.label;
+				loc_elem.value = item.location;
+				name_elem.value = item.name;
+				zip_elem.value = item.zip ?? 'n/a';
+				
+				lat_elem = item.position.lat;
+				lon_elem = item.position.lon;
+
+				let a = 1;
+				let b = 1;
+
+				item.regionalStructure.forEach(index => {
+
+					if (index.type == 'regional.address') {
+						regional_address = index.name;
+					}
+					else if (index.type == 'regional.street') {
+						regional_street = index.name;
+					} 
+					else if (index.type == 'regional.municipality_part') {
+						if (a == 1) {
+							regional_municipality_part_1 = index.name;
+							a++;
+						} else if (a == 2) {
+							regional_municipality_part_2 = index.name;
+						}
+					} 
+					else if (index.type == 'regional.municipality') {
+						regional_municipality = index.name;
+					}
+					else if (index.type == 'regional.region') {	
+						if (b == 1) {
+							regional_region_1 = index.name;
+							b++;
+						} else if (b == 2) {
+							regional_region_2 = index.name;
+						}
+					}
+					else if (index.type == 'regional.country') {
+						regional_country = index.name;
+					}
+					else (index.isoCode) 
+						isoCode = index.isoCode;
+				})
+			})
+			window.dispatchEvent(new CustomEvent('values-updated', {
+				detail: {
+					label: lab_elem.value,
+					location: loc_elem.value,
+					name: name_elem.value,
+					zip: zip_elem.value,
+					lat: lat_elem,
+					lon: lon_elem,
+					regional_address: regional_address,
+					regional_street: regional_street,
+					regional_municipality_part_1: regional_municipality_part_1,
+					regional_municipality_part_2: regional_municipality_part_2,
+					regional_municipality: regional_municipality,
+					regional_region_1: regional_region_1,
+					regional_region_2: regional_region_2,
+					regional_country: regional_country,
+					isoCode: isoCode,
+				}
+			}));
 			// whatever we got from the API, we log to the console
 			console.log(json.items);
-		} else {
-			html = '<p>No results found.</p>';
 		}
-
-		// if we have the answer, we show a popup with some info
-		L.popup()
-			.setLatLng(e.latlng)
-			.setContent(html)
-			.openOn(map)
 	} catch (ex) {
 		// in case of any trouble, we log it to the console
 		console.log(ex);
