b6ef3b6771650cc0ddb3de77e799966260592d41
vit-posmourny <dr1v3r@email.cz>
2025-01-04 01:55:57 +0100

    změnil názvy id

--- resources/views/livewire/elevation-panel.blade.php
+++ resources/views/livewire/elevation-panel.blade.php
@@ -3,19 +3,19 @@
     <form wire:submit.prevent="store" class="flex flex-col">
         {{-- můžu přdat atribut readonly --}}
         <div class="grid grid-cols-12 items-baseline mb-4">
-            <span class="select-none col-span-2">Lat:</span><x-text-input id="inputLatitude" wire:model="latitude" name="latitude" placeholder="Enter Latitude" class="col-span-10"/>
+            <span class="select-none col-span-2">Lat:</span><x-text-input id="i-Latitude" wire:model="latitude" name="latitude" placeholder="Enter Latitude" class="col-span-10"/>
         </div>
         
             <x-input-error class="self-start ml-14" :messages="$errors->get('latitude')"/>
 
         <div class="grid grid-cols-12 items-baseline my-4">        
-            <span class="select-none col-span-2">Lon:</span><x-text-input id="inputLongitude" wire:model="longitude" name="longitude" placeholder="Enter Longitude" class="col-span-10"/> 
+            <span class="select-none col-span-2">Lon:</span><x-text-input id="i-Longitude" wire:model="longitude" name="longitude" placeholder="Enter Longitude" class="col-span-10"/> 
         </div>
 
             <x-input-error class="self-start ml-14" :messages="$errors->get('longitude')"/>
 
         <div class="grid grid-cols-8 items-baseline my-4">
-            <span class="select-none col-span-3">Elevation:</span><x-text-input id="inputElevation" wire:model="elevation" name="elevation" placeholder="Enter Elevation" class="col-span-4 text-right"/><span class="select-none col-span-1 text-right">m</span>
+            <span class="select-none col-span-3">Elevation:</span><x-text-input id="i-Elevation" wire:model="elevation" name="elevation" placeholder="Enter Elevation" class="col-span-4 text-right"/><span class="select-none col-span-1 text-right">m</span>
         </div>
 
             <x-input-error class="self-start ml-14" :messages="$errors->get('elevation')"/>
