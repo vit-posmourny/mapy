2dbd6662d74354edd8ba3bfc0f2bb267e94e5ade
vit-posmourny <dr1v3r@email.cz>
2025-01-04 01:50:55 +0100

    změň názvy id v elevation-panel na i-něco

--- app/Livewire/RgeocodePanel.php
+++ app/Livewire/RgeocodePanel.php
@@ -6,6 +6,36 @@ use Livewire\Component;
 
 class RgeocodePanel extends Component
 {
+    public $label = null;
+    public $location = null;
+    public $name = null;
+    public $zip = null;
+
+    public function store()
+    {
+        $validated = $this->validate([ 
+            
+            'latitude' => 'required|numeric|max:360',
+            'longitude' => 'required|numeric|max:360',
+            'elevation' => 'required|numeric|max:9999',
+        ],[ // pomocí příkazu php artisan vendor:publish --tag=lang
+            // mohu vytáhnout texty s eng a lokalizovat si je do cs
+            'latitude.numeric' => "Hodnota v poli musí být číselná.",
+            'longitude.numeric' => "Hodnota v poli musí být číselná.",
+            'elevation.numeric' => "Hodnota v poli musí být číselná.",
+            'latitude.required' => "Pole nesmí zůstat prázdně.",
+            'longitude.required' => "Pole nesmí zůstat prázdně.",
+            'elevation.required' => "Pole nesmí zůstat prázdně.",
+        ]);
+
+        //znovu nastaví hodnoty všech props.komponenty do init.stavu tj.null
+        $this->reset();
+
+        Place::create($validated);
+
+        session()->flash('success', 'true');
+    }
+
     public function render()
     {
         return view('livewire.rgeocode-panel');
