825b5cddd2a2059ae758667f3eef86bc6d61408f
vit-posmourny <dr1v3r@email.cz>
2025-01-06 20:49:08 +0100

    vytvořím větev pro přidání Geokódování

--- lang/en/validation.php
+++ lang/en/validation.php
@@ -96,7 +96,7 @@ return [
         'array' => 'The :attribute field must not have more than :max items.',
         'file' => 'The :attribute field must not be greater than :max kilobytes.',
         'numeric' => ':attribute nesmí přesáhnout :max.',
-        'string' => 'The :attribute field must not be greater than :max characters.',
+        'string' => 'Pole :attribute, nesmí mít více než :max znaků.',
     ],
     'max_digits' => 'The :attribute field must not have more than :max digits.',
     'mimes' => 'The :attribute field must be a file of type: :values.',
--- resources/views/livewire/elevation-panel.blade.php
+++ resources/views/livewire/elevation-panel.blade.php
@@ -3,24 +3,24 @@
     <form wire:submit.prevent="store" class="flex flex-col">
         {{-- můžu přdat atribut readonly --}}
         <div class="grid grid-cols-12 items-baseline mb-4">
-            <span class="select-none col-span-2">Lat:</span><x-text-input id="i-Latitude" wire:model="latitude" name="latitude" placeholder="Enter Latitude" class="col-span-10"/>
+            <span class="select-none col-span-3">z.šířka:</span><x-text-input id="i-Latitude" wire:model="latitude" name="latitude" placeholder="Latitude" class="col-span-9"/>
         </div>
         
             <x-input-error class="self-start ml-14" :messages="$errors->get('latitude')"/>
 
         <div class="grid grid-cols-12 items-baseline my-4">        
-            <span class="select-none col-span-2">Lon:</span><x-text-input id="i-Longitude" wire:model="longitude" name="longitude" placeholder="Enter Longitude" class="col-span-10"/> 
+            <span class="select-none col-span-3">z.délka:</span><x-text-input id="i-Longitude" wire:model="longitude" name="longitude" placeholder="Longitude" class="col-span-9"/> 
         </div>
 
             <x-input-error class="self-start ml-14" :messages="$errors->get('longitude')"/>
 
         <div class="grid grid-cols-8 items-baseline my-4">
-            <span class="select-none col-span-3">Elevation:</span><x-text-input id="i-Elevation" wire:model="elevation" name="elevation" placeholder="Enter Elevation" class="col-span-4 text-right"/><span class="select-none col-span-1 text-right">m</span>
+            <span class="select-none col-span-3">nadm.výška:</span><x-text-input id="i-Elevation" wire:model="elevation" name="elevation" placeholder="Elevation" class="custom_input col-span-4"/><span class="select-none col-span-1 text-right">m</span>
         </div>
 
             <x-input-error class="self-start ml-14" :messages="$errors->get('elevation')"/>
         
-            <x-submit-button class="mt-4">Store to database</x-submit-button>
+            <x-submit-button class="mt-4">Uložit do databáze</x-submit-button>
 
     </form>
    
