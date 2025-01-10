6cb65662291849ce5525c097130f3070be4b2575
vit-posmourny <dr1v3r@email.cz>
2025-01-10 02:07:29 +0100

    autoComplete.js mně zaobaluje input do autoComplete_wrapperu a je mimo flow sidepanelu

--- resources/views/livewire/geocode-panel.blade.php
+++ resources/views/livewire/geocode-panel.blade.php
@@ -1,13 +1,16 @@
-<div class="w-1/4 shadow-2xl p-8 text-xl font-medium overflow-hidden min-w-96">
+<div class="w-1/4 h-100 shadow-2xl p-8 text-xl font-medium overflow-hidden min-w-96">
 
     <form id="i-geo-form" class="flex flex-col">
 
-        <div class="grid grid-cols-12 items-baseline mb-4">
-            <span class="select-none col-span-3">něco:</span><x-text-input id="" name="search-field" placeholder="něco" class="col-span-9"/>
+        <div class="flex justify-between items-baseline mb-14">
+            
+            <x-input-whisper id="i-search-field" name="search-field"/>
+
+            {{-- <span class="select-none mr-4">něco:</span><input id="i-search-field" type="search" dir="ltr" spellcheck=false autocorrect="off" autocomplete="off" autocapitalize="off"> --}}
+            
         </div>
-
-        <input id="i-search-field" type="search" dir="ltr" spellcheck=false autocorrect="off" autocomplete="off" autocapitalize="off">
-
+        
+        
         <x-submit-button class="mt-4">Hledej</x-submit-button>
         
     </form>
