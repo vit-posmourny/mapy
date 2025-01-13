756113042e83a141ebf8533e62a5af96a3b62dd0
vit-posmourny <dr1v3r@email.cz>
2025-01-13 22:38:00 +0100

    přeložil placeholders do CZ

--- resources/views/livewire/elevation-panel.blade.php
+++ resources/views/livewire/elevation-panel.blade.php
@@ -3,19 +3,19 @@
     <form wire:submit.prevent="store" class="flex flex-col">
         {{-- můžu přdat atribut readonly --}}
         <div class="grid grid-cols-12 items-baseline mb-4">
-            <span class="select-none col-span-3">z.šířka:</span><x-text-input id="i-Latitude" wire:model="latitude" name="latitude" placeholder="Latitude" class="col-span-9"/>
+            <span class="select-none col-span-3">z.šířka:</span><x-text-input id="i-Latitude" wire:model="latitude" name="latitude" placeholder="šířka" class="col-span-9"/>
         </div>
         
             <x-input-error class="self-start ml-14" :messages="$errors->get('latitude')"/>
 
         <div class="grid grid-cols-12 items-baseline my-4">        
-            <span class="select-none col-span-3">z.délka:</span><x-text-input id="i-Longitude" wire:model="longitude" name="longitude" placeholder="Longitude" class="col-span-9"/> 
+            <span class="select-none col-span-3">z.délka:</span><x-text-input id="i-Longitude" wire:model="longitude" name="longitude" placeholder="délka" class="col-span-9"/> 
         </div>
 
             <x-input-error class="self-start ml-14" :messages="$errors->get('longitude')"/>
 
         <div class="grid grid-cols-8 items-baseline my-4">
-            <span class="select-none col-span-3">nadm.výška:</span><x-text-input id="i-Elevation" wire:model="elevation" name="elevation" placeholder="Elevation" class="custom_input col-span-4"/><span class="select-none col-span-1 text-right">m</span>
+            <span class="select-none col-span-3">nadm.výška:</span><x-text-input id="i-Elevation" wire:model="elevation" name="elevation" placeholder="výška" class="custom_input col-span-4"/><span class="select-none col-span-1 text-right">m</span>
         </div>
 
             <x-input-error class="self-start ml-14" :messages="$errors->get('elevation')"/>
