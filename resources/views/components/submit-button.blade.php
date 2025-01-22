0cc6e45009a74a4eb5c6d3ad6ac7fdfee15cd6d5
vit-posmourny <dr1v3r@email.cz>
2025-01-22 16:10:36 +0100

    nefunguje

--- resources/views/components/submit-button.blade.php
+++ resources/views/components/submit-button.blade.php
@@ -2,7 +2,7 @@
 
 <button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center justify-center py-4 text-2xl font-medium tracking-wide text-white transition-colors duration-200 bg-green-600 rounded-lg hover:bg-green-700 focus:ring-2 ring-inset focus:ring-green-800']) }}>
     
-    <div class="flex inline-flex">
+    <div class="inline-flex">
 
         <span> {{ $slot }} </span>  
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,10 +14,10 @@
         {{-- StyleSheets --}}
         <link rel="stylesheet" href="https://fonts.bunny.net/css?family=alex-brush:400|allison:400|bevan:400">
         <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.2/dist/leaflet.css" integrity="sha256-sA+zWATbFveLLNqWO2gtiw3HL/lh1giY/Inf1BJ0z14=" crossorigin="" />
-        <link rel="stylesheet" href="https://unpkg.com/maplibre-gl@^5.0.0/dist/maplibre-gl.css"/>
+        <link rel="stylesheet" href="https://unpkg.com/maplibre-gl@^5.0.1/dist/maplibre-gl.css"/>
         <!-- Scripts -->
         <script src="https://unpkg.com/leaflet@1.9.2/dist/leaflet.js" integrity="sha256-o9N1jGDZrf5tS+Ft4gbIK7mYMipq9lqpVJ91xHSyKhg=" crossorigin=""></script>
-        <script src="https://unpkg.com/maplibre-gl@^5.0.0/dist/maplibre-gl.js"></script>
+        <script src="https://unpkg.com/maplibre-gl@^5.0.1/dist/maplibre-gl.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/@tarekraafat/autocomplete.js@10.2.7/dist/autoComplete.min.js"></script>
         @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/css/autoComplete.css', 'resources/css/webkit.css'])
         <!-- livewireStyles -->
