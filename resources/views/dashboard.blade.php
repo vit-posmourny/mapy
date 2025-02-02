1a87a808b5efae8fbc910862195c5b8dc4a52f13
vit-posmourny <dr1v3r@email.cz>
2025-02-02 21:11:47 +0100

    dashboard screen on mobile ok!

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
 
     <x-slot name="header">
-        <h2 class="font-semibold text-md lg:text-xl text-gray-800 leading-tight">
+        <h2 class="font-semibold text-md sm:text-sm lg:text-xl text-gray-800 leading-tight">
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
@@ -10,15 +10,15 @@
 
         <div class="flex flex-col items-center max-w-7xl mx-auto px-2 lg:px-8">
 
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="bg-white overflow-hidden shadow-sm rounded-lg sm:rounded-md">
 
-                <div class="portrait:text-center font-custom1 text-2xl lg:text-4xl p-6 text-gray-900">
+                <div class="portrait:text-center font-custom1 text-2xl sm:text-xl lg:text-4xl p-5 sm:p-3 lg:p-6 text-gray-900">
                     {{ __("Vítejte na MAPY!") }}
                 </div>
 
             </div>
 
-                <div class="size-1/3 lg:size-1/2 mt-8">
+                <div class="sm:size-1/4 size-1/2 lg:size-1/2 mt-8 sm:mt-3 lg:mt-8">
                     <a href="/">
                         {{-- <img src="images/vecteezy_africa-safari-map-1000x785.png"/> --}}
                         <img src='images/vecteezy_africa-safari-map-1000x785.png'>
