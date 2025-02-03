a99bca3409b06dca4c721e2f2ae2088d11914ad0
vit-posmourny <dr1v3r@email.cz>
2025-02-03 10:34:49 +0100

    add modal window informing that the table is empty

--- resources/views/livewire/modal-table.blade.php
+++ resources/views/livewire/modal-table.blade.php
@@ -6,41 +6,46 @@
         <div class="flex justify-between px-3 pt-1 h-6">
 
             <h2 class="text-xl font-bold select-none">Uložená místa</h2>
-            <button x-on:click="open = false" class="text-lg text-gray-600 hover:text-gray-900">X</button>
+            <button x-on:click="open = false" class="text-lg ml-3 text-gray-600 hover:text-gray-900">X</button>
 
         </div>
 
         <template x-if="open">
             <!-- Modal Table -->
             <div class="mt-4 max-h-[50vh] lg:max-h-[25vh] overflow-auto scroll-smooth modal_table">
+            
+                @if ($data)
+                    <table class="text-base border-collapse border border-slate-400">
 
-                <table class="text-base border-collapse border border-slate-400">
+                        <thead class="font-bold text-lg text-green-900 bg-green-200">
+                            <tr>
+                                <th class="border px-2 border-slate-400">Druh</th>
+                                <th class="border px-2 border-slate-400">Lokace</th>
+                                <th class="border px-2 border-slate-400">Název</th>
+                                <th class="border px-2 border-slate-400">PSČ</th>
+                                <th class="border px-2 border-slate-400">Země</th>
+                                <th class="border px-2 border-slate-400">Z.Šířka</th>
+                                <th class="border px-2 border-slate-400">Z.Délka</th>
+                            </tr>
+                        </thead>
+                        <tbody class="text-nowrap">
+                        @foreach ($data as $row)
+                            <tr>
+                                <td class="border text-center px-2 border-slate-400">{{ $row['label'] }}</td>
+                                <td class="border text-center px-2 border-slate-400">{{ $row['location'] }}</td>
+                                <td class="border text-center px-2 border-slate-400">{{ $row['name'] }}</td>
+                                <td class="border text-center px-2 border-slate-400">{{ $row['zip'] }}</td>
+                                <td class="border text-center px-2 border-slate-400">{{ $row['isoCode'] }}</td>
+                                <td class="border text-center px-2 border-slate-400">{{ $row['latitude'] }}</td>
+                                <td class="border text-center px-2 border-slate-400">{{ $row['longitude'] }}</td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                @else 
+                    <p class="text-center py-2">Tabulka je prázná.</p>
+                @endif
 
-                    <thead class="font-bold text-lg text-green-900 bg-green-200">
-                        <tr>
-                            <th class="border px-2 border-slate-400">Druh</th>
-                            <th class="border px-2 border-slate-400">Lokace</th>
-                            <th class="border px-2 border-slate-400">Název</th>
-                            <th class="border px-2 border-slate-400">PSČ</th>
-                            <th class="border px-2 border-slate-400">Země</th>
-                            <th class="border px-2 border-slate-400">Z.Šířka</th>
-                            <th class="border px-2 border-slate-400">Z.Délka</th>
-                        </tr>
-                    </thead>
-                    <tbody class="text-nowrap">
-                    @foreach ($data as $row)
-                        <tr>
-                            <td class="border text-center px-2 border-slate-400">{{ $row['label'] }}</td>
-                            <td class="border text-center px-2 border-slate-400">{{ $row['location'] }}</td>
-                            <td class="border text-center px-2 border-slate-400">{{ $row['name'] }}</td>
-                            <td class="border text-center px-2 border-slate-400">{{ $row['zip'] }}</td>
-                            <td class="border text-center px-2 border-slate-400">{{ $row['isoCode'] }}</td>
-                            <td class="border text-center px-2 border-slate-400">{{ $row['latitude'] }}</td>
-                            <td class="border text-center px-2 border-slate-400">{{ $row['longitude'] }}</td>
-                        </tr>
-                    @endforeach
-                    </tbody>
-                </table>
             </div>
         </template>
         <!-- Modal Buttons -->
