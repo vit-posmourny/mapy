5837f45cbd1fea408a374c964b35dd5cfe9396e3
vit-posmourny <dr1v3r@email.cz>
2025-02-16 01:42:53 +0100

    improve responsibility on 'profile' path in landscape orientation

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -6,7 +6,7 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
+    <div class="py-1 lg:py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
             <div class="p-4 sm:p-8 bg-dark_wood border border-yellow-800 shadow-md overflow-hidden sm:rounded-lg">
                 <div class="max-w-xl">
