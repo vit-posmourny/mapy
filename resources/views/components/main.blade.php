8cac7724f5f7dc4131603eaa9ab549a12220c270
vit-posmourny <dr1v3r@email.cz>
2025-02-16 01:42:53 +0100

    improve responsibility on 'profile' path in landscape orientation

--- resources/views/components/main.blade.php
+++ resources/views/components/main.blade.php
@@ -1,7 +1,18 @@
 @php
-$classes = request()->path() == 'dashboard'
-            ? 'h-[92.5vh] lg:h-auto flex landscape:flex portrait:flex-col lg:justify-center'
-            : 'h-[92.5vh] flex landscape:flex portrait:flex-col lg:justify-center';       
+switch (request()->path()) {
+
+    case 'dashboard':
+        $classes = 'h-[92.5vh] lg:h-auto flex landscape:flex portrait:flex-col lg:justify-center';
+        break;
+
+    case 'profile':
+        $classes = 'h-[92.5vh] flex landscape:flex landscape:mx-auto landscape:w-3/4 landscape:place-content-center portrait:flex-col lg:justify-center';
+        break;
+    
+    default:
+        $classes = 'h-[92.5vh] flex landscape:flex portrait:flex-col lg:justify-center';
+        break;
+}      
 @endphp
 
 <main  {{ $attributes->merge(['class' => $classes]) }}>
