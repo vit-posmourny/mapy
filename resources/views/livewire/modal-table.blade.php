5bf8cf0629f7890befebdb12ed4e917a96ee7ed7
vit-posmourny <dr1v3r@email.cz>
2025-04-06 01:21:57 +0200

    change rowId[ ] to rowIds[ ] in modal-table.js

--- resources/views/livewire/modal-table.blade.php
+++ resources/views/livewire/modal-table.blade.php
@@ -59,7 +59,7 @@
                     <!-- Modal buttons -->
                     <div class="flex mt-4 justify-end">
                         {{-- Delete button --}}
-                        <x-delete-button x-bind:disabled="$store.Row.rowId === null" wire:click="delete($store.Row.rowId)" class="mb-1"></x-delete-button>
+                        <x-delete-button x-bind:disabled="$store.Row.rowIds.lenght === 0" wire:click="delete($store.Row.rowIds)" class="mb-1"></x-delete-button>
                         {{-- Close button --}}
                         <x-non-submit-button x-on:click="open = false" class="mx-2 mb-1">Close</x-non-submit-button>
                     </div>
